bf91f798303bef7f783eab5cd23a6458260b510d
lxq <lxq271332@alibaba-inc.com>
2025-12-23 16:33:30 +0800

    feat: add connector interface & connector coordinator

--- rtp_llm/cpp/engine_base/stream/StreamCacheResource.cc
+++ rtp_llm/cpp/engine_base/stream/StreamCacheResource.cc
@@ -11,7 +11,9 @@ namespace rtp_llm {
 
 void StreamCacheResource::init(int batch_size) {
     batch_kv_cache_resource_->resetBatchSize(batch_size);
-    batch_kv_cache_resource_->initGroups(1);
+    RTP_LLM_CHECK_WITH_INFO(resource_context_.cache_manager != nullptr, "cache manager is nullptr!");
+    int layer_num = resource_context_.cache_manager->cacheConfig().layer_num;
+    batch_kv_cache_resource_->initGroups(1, layer_num);
     batch_kv_cache_resource_->enable_reuse_cache = reuseCache();
 }
 
